0726e4939cf5ad982132711bdda4c21a6a923500
MOHOAzure <timecrutramer@gmail.com>
2021-12-03 22:05:36 +0800

    Update shutdown.sh

--- Auto stop server/shutdown.sh
+++ Auto stop server/shutdown.sh
@@ -6,9 +6,9 @@
 #This will stop the EC2 instance and save money.
 
 # redirect stdout/stderr to a file
-LOG_LOCATION=./logs
+LOG_LOCATION=/var/minecraft/server/1_18/scripts/logs
 LOG_NAME=$(date '+%Y-%m-%d').txt
-exec >>$LOG_LOCATION/"$LOG_NAME" 2>&1
+exec >>$LOG_LOCATION/$LOG_NAME 2>&1
 
 check_port_conn(){
   sshCons=$(netstat -anp | grep :22 | grep ESTABLISHED | wc -l)
@@ -17,20 +17,26 @@ check_port_conn(){
   echo "$(date '+%Y-%m-%d %H:%M:%S') Active SSH Connections: $sshCons"
   echo "$(date '+%Y-%m-%d %H:%M:%S') Active Minecraft Connections: $mcCons"
 
-  if [[ $((sshCons)) != 0 ]]
+  is_admin_working=false
+  if [ "$sshCons" != 0 ]
   then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') There are 1 or more active ssh connections, skip termination"
-    return 0
-  fi
-  if [ $((mcCons)) != 0 ]
-  then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') Somebody is playing minecraft, skip termination"
-    return 0
+    is_admin_working=true
+    echo "$(date '+%Y-%m-%d %H:%M:%S') There are 1 or more active ssh connections"
   fi
 
-  echo "$(date '+%Y-%m-%d %H:%M:%S') No ssh connections, shutting down server"
-  sudo systemctl stop minecraft
-  sudo shutdown
+  is_playing=false
+  if [ "$mcCons" != 0 ]
+  then
+    is_playing=true
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Somebody is playing minecraft"
+  fi
+
+  if [ "$is_admin_working" == false ] && [ "$is_playing" == false ]
+  then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') No connections, backup & shutting down server"
+    sudo systemctl stop minecraft
+    sudo shutdown
+  fi
 }
 
 check_port_conn
