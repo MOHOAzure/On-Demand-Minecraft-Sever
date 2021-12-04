ca29118e8f81b444647c14d823a44bbffca0dc24
MOHOAzure <timecrutramer@gmail.com>
2021-12-04 13:15:47 +0800

    Update shutdown_backup.sh

--- Backup/shutdown_backup.sh
+++ Backup/shutdown_backup.sh
@@ -34,17 +34,24 @@ check_port_conn(){
 
   if [ "$is_admin_working" == false ] && [ "$is_playing" == false ]
   then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') No connections, backup & shutting down server"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') No connections, Shutting down server"
     sudo systemctl stop minecraft
+    backup
+    sudo shutdown
+  fi
+}
+
+backup(){
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Game Server Backup"
+
+    # go to game server dir
+    cd /var/minecraft/server
 
     # zip for space saving
-    sudo zip backup.zip /var/minecraft/server/1_18/*
+    sudo zip -r -q backup.zip 1_18
 
     # store backup in S3
     sudo aws s3 mv backup.zip s3://mc-server-backup-e0470/1_18/backup.zip
-
-    sudo shutdown
-  fi
 }
 
 check_port_conn
